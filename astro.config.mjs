0ef2bf8b6edde1888d7db9ba94369d3dad6b4977
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2023-12-27 21:35:53 -0600

    fix navbar bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
