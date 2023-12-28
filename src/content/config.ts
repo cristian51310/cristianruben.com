87f06c6d9bd615f2b653c89ef44219421ae594ea
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2023-12-27 20:19:21 -0600

    delete mds of projects and change for ts file

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,16 +10,4 @@ const blog = defineCollection({
 	}),
 });
 
-const projects = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		webpage: z.string().optional(),
-		github: z.string().optional(),
-		pubDate: z.coerce.date(),
-		heroImage: z.string().optional(),
-	}),
-});
-
-export const collections = { blog, projects };
+export const collections = { blog };
---
+++ src/data/projects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: 'Kuil',
+    description: 'Pagina web ecommerce y dashboard de administracion para Kuil, una marca de lapices ecológicos',
+    pubDate: 'Jul 08 2022',
+    heroImage: '/images/projects/kuil/ss.webp',
+    webpage: 'https://kuil.com.mx',
+    github: null
+  },
+  {
+    title: 'Portafolio',
+    description: 'Mi portafolio personal de codigo abierto en el cual muestro mis proyectos y articulos de blog',
+    pubDate: 'Jul 08 2022',
+    heroImage: '/images/projects/portfolio/ss.webp',
+    webpage: 'https://cristianfigueroa.vercel.app/',
+    github: 'https://github.com/cristian51310/cristianruben.com'
+  },
+  {
+    title: 'Ring!',
+    description: 'Un marketplace digital enfocado a restaurantes, el cual permite comprar comida a domicilio (aun en desarrollo)',
+    pubDate: 'Jul 08 2023',
+    heroImage: '/images/projects/ring/ss.webp',
+    webpage: 'https://ring-eccommerce.vercel.app/',
+    github: null
+  }
+]
