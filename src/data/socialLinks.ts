8907d298ad36cace0e6ce7345affa24d57602565
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2023-12-29 14:59:01 -0600

    fix accessibility in social icons

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -3,25 +3,30 @@ export const socialLinks = [
     name: "Github",
     url: "https://github.com/cristian51310",
     icon: "mdi:github",
+    label: "Go to my github"
   },
   {
     name: "Linkedin",
     url: "https://www.linkedin.com/in/cristian-ruben-figueroa-espinoza-613245262/",
     icon: "mdi:linkedin",
+    label: "Go to my linkedin"
   },
   {
     name: "Whatsapp",
     url: "https://wa.me/+524621493816",
     icon: "mdi:whatsapp",
+    label: "Go to my whatsapp"
   },
   {
     name: "Twitter",
     url: "https://twitter.com/CristianRubnFi1",
     icon: "mdi:twitter",
+    label: "Go to my twitter"
   },
   {
     name: "Email",
     url: "mailto:cristian.figueroa.crfe@gmail.com",
     icon: "mdi:email",
+    label: "Go to my email"
   },
 ];
