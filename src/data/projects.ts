b6287ad0833fcadf09ccfb00137ffa4c0e0ead6e
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2023-12-29 19:54:44 -0600

    update projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -14,18 +14,6 @@ export const projects = [
     tags: [TAGS.REACT, TAGS.NEXTJS, TAGS.JS]
   },
   {
-    title: 'Portafolio',
-    description: {
-      es: 'Mi portafolio personal de codigo abierto en el cual muestro mis proyectos y articulos de blog hecho con Astro y TailwindCSS y multilenaguaje',
-      en: 'My personal open source portfolio in which I show my projects and blog articles made with Astro and TailwindCSS and multilingual'
-    },
-    pubDate: 'Jul 08 2022',
-    heroImage: '/images/projects/portfolio.webp',
-    webpage: 'https://cristianfigueroa.vercel.app/',
-    github: 'https://github.com/cristian51310/cristianruben.com',
-    tags: [TAGS.VERCEL, TAGS.ASTRO, TAGS.TAILWIND]
-  },
-  {
     title: 'Ring!',
     description: {
       es: "Un marketplace digital enfocado a restaurantes, que permite comprar comida a domicilio (aún en desarrollo)",
