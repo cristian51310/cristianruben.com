8b0f7699b45a5a1a3e7ed10fb6931b8de62d17a1
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2023-12-31 14:17:36 -0600

    replace navbar in react for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import mdx from '@astrojs/mdx';
-import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://cristianfigueroa.dev',
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
