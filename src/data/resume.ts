8899879f974e1c2b8ef1522ce224a3ac86959e80
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-02-22 00:39:56 -0600

    new styles in projects sections

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,3 +1,5 @@
+import { TAGS } from "./tags"
+
 export const resume = {
   education: [
     {
@@ -60,6 +62,56 @@ export const resume = {
         ]
       }
     },
+  ],
+  projects: [
+    {
+      title: 'Kuil',
+      description: {
+        es: 'Pagina web ecommerce y dashboard de administracion para Kuil, una marca de lapices ecológicos',
+        en: 'Ecommerce website and administration dashboard for Kuil, an ecological pencil brand'
+      },
+      pubDate: 'Jul 08 2022',
+      heroImage: '/images/projects/kuil.webp',
+      webpage: 'https://kuil.com.mx',
+      github: null,
+      tags: [TAGS.REACT, TAGS.NEXTJS, TAGS.JS]
+    },
+    {
+      title: 'Dorichangos',
+      description: {
+        es: 'Sistema de reservaciones y comercio electrónico. Permite reservar mesas y hacer pedidos en línea.',
+        en: 'Reservation and e-commerce system. Allows customers to book tables and place orders online.'
+      },
+      pubDate: 'Dec 08 2023',
+      heroImage: '/images/projects/dorichangos.webp',
+      webpage: 'https://dorichangos.vercel.app',
+      github: null,
+      tags: [TAGS.REACT, TAGS.NEXTJS, TAGS.TS, TAGS.VERCEL]
+    },
+    {
+      title: 'TeamPilot',
+      description: {
+        es: 'Aplicación de gestión de proyectos basada en la metodología Kanban, soporte cooperativo para trabajar en equipo y asignar responsabilidades.',
+        en: 'Project management application based on the Kanban methodology, cooperative support for teamwork and assigning responsibilities.'
+      },
+      pubDate: 'Jan 08 2024',
+      heroImage: '/images/projects/team-pilot.webp',
+      webpage: 'https://team-pilot.vercel.app',
+      github: null,
+      tags: [TAGS.REACT, TAGS.NEXTJS, TAGS.TS, TAGS.VERCEL]
+    },
+  ],
+  stack: [
+    TAGS.REACT,
+    TAGS.ASTRO,
+    TAGS.NEXTJS,
+    TAGS.JS,
+    TAGS.TS,
+    TAGS.PRISMA,
+    TAGS.NODEJS,
+    TAGS.GIT,
+    TAGS.GITHUB,
+    TAGS.TAILWIND,
   ],
   skills: [
     {
