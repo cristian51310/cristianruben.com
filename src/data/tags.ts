42cba36a1b3e29b603be85c2a2f886b75e837c05
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-02-22 22:34:31 -0600

    add base url with relative path

--- src/data/tags.ts
+++ src/data/tags.ts
@@ -1,15 +1,15 @@
-import Astro from "../components/icons/AstroJS.astro"
-import Git from "../components/icons/Git.astro"
-import Github from "../components/icons/Github.astro"
-import JavaScript from "../components/icons/JavaScript.astro"
-import Nest from "../components/icons/Nest.astro"
-import NextJS from "../components/icons/NextJS.astro"
-import Node from "../components/icons/Node.astro"
-import Prisma from "../components/icons/Prisma.astro"
-import React from "../components/icons/React.astro"
-import TailwindCSS from "../components/icons/TailwindCSS.astro"
-import TypeScript from "../components/icons/TypeScript.astro"
-import Vercel from "../components/icons/Vercel.astro"
+import Astro from "@/components/icons/AstroJS.astro"
+import Git from "@/components/icons/Git.astro"
+import Github from "@/components/icons/Github.astro"
+import JavaScript from "@/components/icons/JavaScript.astro"
+import Nest from "@/components/icons/Nest.astro"
+import NextJS from "@/components/icons/NextJS.astro"
+import Node from "@/components/icons/Node.astro"
+import Prisma from "@/components/icons/Prisma.astro"
+import React from "@/components/icons/React.astro"
+import TailwindCSS from "@/components/icons/TailwindCSS.astro"
+import TypeScript from "@/components/icons/TypeScript.astro"
+import Vercel from "@/components/icons/Vercel.astro"
 
 export const TAGS = {
   NEXTJS: {
@@ -48,11 +48,11 @@ export const TAGS = {
     name: 'Node.js',
     icon: Node
   },
-  GIT:{
+  GIT: {
     name: 'Git',
     icon: Git
   },
-  GITHUB:{
+  GITHUB: {
     name: 'GitHub',
     icon: Github
   },
