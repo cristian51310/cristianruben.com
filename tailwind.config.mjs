29a6fc45c454526e13e89ba53092d633117c76c7
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-02-23 18:29:20 -0600

    add buttons for colortheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,js,jsx,md,mdx,ts,tsx}'],
 	darkMode: "class",
-	theme: {},
+	theme: {
+		extend: {
+			colors: {
+				"bg-color": "var(--bg-color)",
+				"bg-purple": "var(--bg-purple)",
+				"bg-green": "var(--bg-green)",
+				"bg-yellow": "var(--bg-yellow)",
+			},
+		},
+	},
 	plugins: [],
 }
